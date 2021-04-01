f2f689c88cc481c78fba2288ae628bb5e8f078df
nizarst <61068117+nizarst@users.noreply.github.com>
2021-04-01 01:19:09 +0100

    Add sub method

--- pubsub.py
+++ pubsub.py
@@ -51,3 +51,55 @@ def pub():
     message_id = api_future.result()
 
     #print(f"Published {data} to {topic_path}: {message_id}")
+    
+    def sub(project_id, subscription_id, timeout=None):
+    """Receives messages from a Pub/Sub subscription."""
+    # Initialize a Subscriber client
+    subscriber_client = pubsub_v1.SubscriberClient()
+    # Create a fully qualified identifier in the form of
+    # projects/{project_id}/subscriptions/{subscription_id}
+    subscription_path = subscriber_client.subscription_path(project_id, subscription_id)
+
+
+    def callback(message):
+        print(f"Received {message.data}.")
+        if message.attributes:
+            print("Attributes:")
+            value = message.attributes.get("P-MOB")
+            if value == "MOB-9e31cf6d-1c80-44a0-b3f3-c0c5c02c23ad":
+                pub()
+        message.ack()
+
+
+    streaming_pull_future = subscriber_client.subscribe(
+        subscription_path, callback=callback)
+
+
+
+    print(f"Listening for messages on {subscription_path}..\n")
+
+    try:
+        # Calling result() on StreamingPullFuture keeps the main thread from
+        # exiting while messages get processed in the callbacks.
+        streaming_pull_future.result(timeout=timeout)
+
+    except:  # noqa
+        streaming_pull_future.cancel()
+
+    subscriber_client.close()
+
+
+if name == "main":
+    """parser = argparse.ArgumentParser(
+        description=doc, formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument("project_id", help="Google Cloud project ID")
+    parser.add_argument("subscription_id", help="Pub/Sub subscription ID")
+    parser.add_argument(
+        "timeout", default=None, nargs="?", const=1, help="Pub/Sub subscription ID"
+    )
+
+    args = parser.parse_args()
+"""
+    sub(project_id, subscription_id)
+
